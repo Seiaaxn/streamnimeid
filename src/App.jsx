89cd5aad7e5e95d06de23e1989687693dc86c21f
Shin <ryu694602@gmail.com>
2026-02-21 21:25:12 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+// src/App.jsx
+import { Routes, Route } from 'react-router-dom';
+import MainLayout from './components/layout/MainLayout';
+import HomePage from './pages/HomePage';
+import DetailPage from './pages/DetailPage/index';
+import StreamingAnime from './pages/Streaming/StreamingAnime';
+import StreamingDonghua from './pages/Streaming/StreamingDonghua';
+import ExplorerPage from './pages/ExplorerPage';
+import MyListPage from './pages/MyListPage';
+import ProfilePage from './pages/ProfilePage';
+import AutoToTop from './components/AutoToTop';
+import Search from './pages/Search';
+
+function App() {
+  return (
+    <>
+      <AutoToTop />
+      <Routes>
+        <Route path="/detail/:category/:id" element={<DetailPage />} />
+        <Route path="/anime/watch" element={<StreamingAnime />} />
+        <Route path="/donghua/watch" element={<StreamingDonghua />} />
+
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/explore" element={<ExplorerPage />} />
+          <Route path="/mylist" element={<MyListPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/search" element={<Search />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
